13b8ec826aac8f932cb98381f840132235a8049b
deng-rui <deng-rui@foxmail.com>
2021-07-11 21:00:58 +0800

    1.3.0-M2-DEV

--- src/main/java/example/Event.java
+++ src/main/java/example/Event.java
@@ -1,7 +1,6 @@
 package example;
 
 import com.github.dr.rwserver.data.Player;
-import com.github.dr.rwserver.net.core.AbstractNetConnect;
 import com.github.dr.rwserver.plugin.event.AbstractEvent;
 import com.github.dr.rwserver.util.log.Log;
 
@@ -15,15 +14,4 @@ public class Event implements AbstractEvent {
     public void registerPlayerJoinEvent(Player player) {
         player.sendSystemMessage("你好!! 这是RW-HPS新的Event的实现");
     }
-
-    /**
-     * 这是目前的1.3.0 -M1的bug
-     * 如果要使用新的Event实现 请带上这个覆盖
-     */
-    @Override
-    public String[] registerPlayerConnectPasswdCheckEvent(AbstractNetConnect abstractNetConnect, String passwd) {
-        return new String[]{"false",""};
-    }
-
-
 }
