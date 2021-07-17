584a27accb355f58f968b5fd66769a0781d0f233
deng-rui <deng-rui@foxmail.com>
2021-07-18 07:05:30 +0800

    1.3.0

--- src/main/java/example/Event.java
+++ src/main/java/example/Event.java
@@ -4,6 +4,13 @@ import com.github.dr.rwserver.data.Player;
 import com.github.dr.rwserver.plugin.event.AbstractEvent;
 import com.github.dr.rwserver.util.log.Log;
 
+/**
+ * 这个是新的Event的实现
+ * 如果要使用.我推荐使用新的实现
+ * 因为新的实现有同步和异步
+ * 旧的事件是同步的 如果进行高IO 那么游戏体验或许极差
+ * @author Dr
+ */
 public class Event implements AbstractEvent {
     @Override
     public void registerServerLoadEvent() {
--- src/main/java/example/Main.java
+++ src/main/java/example/Main.java
@@ -9,6 +9,7 @@ import com.github.dr.rwserver.plugin.event.AbstractEvent;
 import com.github.dr.rwserver.util.Time;
 import com.github.dr.rwserver.util.game.CommandHandler;
 import com.github.dr.rwserver.util.game.Events;
+import com.github.dr.rwserver.util.log.Log;
 
 import java.util.Arrays;
 
@@ -16,6 +17,9 @@ import java.util.Arrays;
  * @author Dr
  */
 public class Main extends Plugin {
+	/**
+	 * 这里主要做初始化
+	 */
 	@Override
 	public void init(){
 		//监听玩家进入
@@ -27,6 +31,31 @@ public class Main extends Plugin {
 		Data.core.admin.addChatFilter((player, text) -> text.replace("heck", "h*ck"));
 
 		//动作过滤正在进行中
+
+		/**
+		 * java.io.EOFException
+		 *         at java.base/java.util.zip.GZIPInputStream.readUByte(GZIPInputStream.java:268)
+		 *         at java.base/java.util.zip.GZIPInputStream.readUShort(GZIPInputStream.java:258)
+		 *         at java.base/java.util.zip.GZIPInputStream.readHeader(GZIPInputStream.java:164)
+		 *         at java.base/java.util.zip.GZIPInputStream.<init>(GZIPInputStream.java:79)
+		 *         at java.base/java.util.zip.GZIPInputStream.<init>(GZIPInputStream.java:91)
+		 *         at com.github.dr.rwserver.util.zip.gzip.GzipDecoder.getGzipInputStream(GzipDecoder.java:23)
+		 *         at com.github.dr.rwserver.data.plugin.AbstractPluginData.read(AbstractPluginData.java:84)
+		 *         at com.github.dr.rwserver.data.plugin.AutoSavePluginData.read(AutoSavePluginData.java:6)
+		 *         at com.github.dr.rwserver.plugin.PluginsLoad$PluginLoadData.<init>(PluginsLoad.java:107)
+		 *         at com.github.dr.rwserver.plugin.PluginsLoad.loadJar(PluginsLoad.java:52)
+		 *         at com.github.dr.rwserver.plugin.PluginsLoad.resultPluginData(PluginsLoad.java:29)
+		 *         at com.github.dr.rwserver.data.plugin.PluginManage.init(PluginManage.java:27)
+		 *         at com.github.dr.rwserver.Main.main(Main.java:79)
+		 *         这个是正常的 正在想办法解决
+		 */
+		//读取数据
+		long lastStartTime = this.getPluginData().getData("lastStartTime",Time.concurrentMillis());
+		String lastStartTimeString = this.getPluginData().getData("lastStartTimeString",Time.getUtcMilliFormat(1));
+		Log.info("lastStartTime",lastStartTime);
+		Log.info("lastStartTimeString",lastStartTimeString);
+		this.getPluginData().setData("lastStartTime",Time.concurrentMillis());
+		this.getPluginData().setData("lastStartTimeString",Time.getUtcMilliFormat(1));
 	}
 
 	@Override
@@ -71,5 +100,11 @@ public class Main extends Plugin {
 			//向玩家发消息
 			other.sendSystemMessage("玩家: " + player.name + " 向你发送: " + args[1]);
 		});
+	}
+
+	@Override
+	public void onDisable() {
+		super.onDisable();
+		//Custom
 	}
 }
